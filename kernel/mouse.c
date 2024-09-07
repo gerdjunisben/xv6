e127d0d66c65755513b8799d4a91f83d38de74c5
Benjamin Gerdjunis <benjamingerdjunis@gmail.com>
2024-09-07 10:18:45 -0400

    Mouse buffer created, misaligned but working.
    Co-authored-by: Santiago Ortiz Betancourt <sortizb@users.noreply.github.com>

--- kernel/mouse.c
+++ kernel/mouse.c
@@ -2,11 +2,67 @@
 #include "defs.h"
 #include "x86.h"
 #include "mouse.h"
+#include "spinlock.h"
+#include "sleeplock.h"
 
+static struct {
+    uint buffer[9];
+    int producerIndex;
+    int consumerIndex;
+    uint n;
+    uint size;
+    struct sleeplock sleepLock;
+    struct spinlock spinLock;
+} msBuffer;
+
+void consume(uint* packet,uint size)
+{
+    acquire(&msBuffer.spinLock);
+
+    if(size<3)
+    {
+        return;
+    }
+
+    if(msBuffer.size<3)
+    {
+        acquiresleep(&msBuffer.sleepLock);
+    }
+
+    packet[0] = msBuffer.buffer[(msBuffer.consumerIndex++) % msBuffer.n];
+    packet[1] = msBuffer.buffer[(msBuffer.consumerIndex++) % msBuffer.n];
+    packet[2] = msBuffer.buffer[(msBuffer.consumerIndex++) % msBuffer.n];
+    
+    msBuffer.size -= 3;
+
+    release(&msBuffer.spinLock);
+}
+
+void produce(uint msg)
+{
+    acquire(&msBuffer.spinLock);
+    
+    if(msBuffer.size != 9)
+    {
+        msBuffer.buffer[(msBuffer.producerIndex++) % msBuffer.n] = msg;
+        msBuffer.size++;
+
+        if(msBuffer.size>=3)
+            releasesleep(&msBuffer.sleepLock);
+    }
+
+    release(&msBuffer.spinLock);
+}
 
 void mouseinit(void)
 {
     //this is where we init buffer :)
+    initsleeplock(&msBuffer.sleepLock, "ms_sleeplock");
+    initlock(&msBuffer.spinLock, "ms_sleeplock");
+    msBuffer.producerIndex = 0;
+    msBuffer.consumerIndex = 0;
+    msBuffer.size = 0;
+    msBuffer.n = 9;
     
     //whenever we read or write add a timeout
 
@@ -77,6 +133,17 @@ void mouseintr(void)
     else
     {
         uint data = inb(MSDATAP);
-        cprintf("Mouse data packet %x\n",data);
+        produce(data);
+
+        if(msBuffer.size>=3)
+        {
+            uint res[3];
+            consume(res,3);
+            for(int i =0;i<3;i++)
+            {
+                cprintf("Packet %d is %x ",i,res[i]);
+            }
+            cprintf("\n");
+        }
     }
 }
--- kernel/trap.c
+++ kernel/trap.c
@@ -36,11 +36,11 @@ idtinit(void)
 void
 trap(struct trapframe *tf)
 {
-  
+  /*
   if(tf->trapno != T_IRQ0 + IRQ_TIMER)
   {
     cprintf("Trapno %d",tf->trapno);
-  }
+  }*/
   if(tf->trapno == T_SYSCALL){
     if(myproc()->killed)
       exit();
