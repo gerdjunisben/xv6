bfbf9882f6b4a4f6b5d0153d69370c7c8bf48a7b
Santiago Ortiz Betancourt <santiago.ortizbetancourt@stonybrook.edu>
2024-10-27 02:40:27 -0400

    almost done with data

--- user/proc_test.c
+++ user/proc_test.c
@@ -1,78 +1,43 @@
 #include "kernel/types.h"
 #include "user.h"
 
-static int sum = 1;
-
-void setSum(uint i)
-{
-    sum = i;
-}
-
-void mulSum(uint i)
-{
-    sum*=i;
-}
-
-void addSum(uint i)
-{
-    sum+=i;
-}
-
-void compute(float j)
-{
-    for(int i = 0;i < 10000 * j; i++)
-    {
-        addSum(i);
-        mulSum(i);
-        addSum(i);
-        mulSum(i);
-        mulSum(i);
-        mulSum(i);     
+int compute(int num_iter, int sleep_time) {
+    int final = 0;
+    for (int i = 0; i < num_iter; i++) {
+        for(int j = 0; j < 14600000; j++) {
+            final += j * i;
+        }
+        sleep(sleep_time);
     }
-}
-
-void cpu_mode(uint pid, int num_iter) {
-    for(int j = 0; j < num_iter; j++) {
-       compute(j);
-       sleep(5);
-    }
-}
-
-void io_mode(uint pid, int num_iter) {
-    for(int j = 0; j < num_iter; j++) {
-       compute(j);
-       sleep(70);
-    }
-}
-
-void fifty_fifty_mode(uint pid, int num_iter) {
-    for(int j = 0; j < num_iter; j++) {
-       compute(j);
-       sleep(50);
-    }
+    return final;
 }
 
 void mode_router(int mode, int pid, int num_iter) {
+    int final = 0;
+
+    //cpu burst mode
     if (mode == 0) {
-        cpu_mode(pid, num_iter);
+        final = compute(num_iter, 0);
     }
 
+    //I/O burst mode
     else if (mode == 1) {
-        io_mode(pid, num_iter);
+        final = compute(num_iter, 3);
     }
 
+    // 50/50 mode
     else {
-        fifty_fifty_mode(pid, num_iter);
+        final = compute(num_iter, 1);
     }
 
     // parent process
     if (pid == -9) {
-        printf(0,"> parent process finished\n> sum = %d\n", sum);
+        printf(0,"> parent process finished\n> final = %d\n", final);
         exit();
     }
     // child process
     else {
-        printf(0, "> process %d exited\n> sum = %d\n", pid, sum);
+        printf(0, "> process %d exited\n> final = %d\n", pid, final);
         exit();
     }
 }
@@ -85,7 +50,7 @@ int validargs(int argc, char *argv[]) {
     int num_iter = atoi(argv[2]);
     int mode = atoi(argv[3]);
 
-    if (num_child < 0 || num_child > 20 || num_iter < 0 || num_iter > 200 || mode < 0 || mode > 2)
+    if (num_child < 0 || num_child > 20 || num_iter < 0 || num_iter > 50000 || mode < 0 || mode > 2)
         return -1;
     
     return 0;
@@ -101,7 +66,7 @@ int main(int argc, char *argv[])
 {
 
    if (validargs(argc, argv) == -1) {
-    printf(0, "> invalid args\n> ussage:\n  1st arg = num of children to fork [0-20]\n  2nd arg = num iterations [0-200]\n  3rd arg = mode [0: cpu burst mode, 1: i/o burst mode, 2: 50/50 mode]\n");
+    printf(0, "> invalid args\n> ussage:\n  1st arg = num of children to fork [0-20]\n  2nd arg = num iterations [0-50000]\n  3rd arg = mode [0: cpu burst mode, 1: i/o burst mode, 2: 50/50 mode]\n");
     exit();
    }
 
